c84ddef8942c2f50513cdc89bb87b530ef545110
zeeshan_shakil <zshan.shakil@gmail.com>
2018-01-19 15:21:05 +0500

    Small CHange

--- index.php
+++ index.php
@@ -1091,7 +1091,7 @@
     </div>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
